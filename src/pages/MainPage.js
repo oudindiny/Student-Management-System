a16bd7205b3e95b7765e6c4cf6653b1e2227d833
yujin <miru93198@gmail.com>
2023-02-12 14:28:39 +0900

    MainPage 수정

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -20,15 +20,39 @@ const MainPage = () => {
           <span>로그아웃</span>
         </div>
       </div>
-      <div className="table">
-        <div className="no">
-          <p>{tableData.map((it) => it.no)}</p>
+      <div className="tables">
+        <div className="table color_gray">
+          <div className="no">
+            <p>No</p>
+          </div>
+          <div className="bookName">
+            <p>책 제목</p>
+          </div>
+          <div className="readDate">
+            <p>대여 일자</p>
+          </div>
         </div>
-        <div className="bookName">
-          <p>{tableData.map((it) => it.bookName)}</p>
+        <div className="table">
+          <div className="no">
+            <p>{tableData[0].no}</p>
+          </div>
+          <div className="bookName">
+            <p>{tableData[0].bookName}</p>
+          </div>
+          <div className="readDate">
+            <p>{tableData[0].readDate}</p>
+          </div>
         </div>
-        <div className="readDate">
-          <p>{tableData.map((it) => it.readDate)}</p>
+        <div className="table2">
+          <div className="no">
+            <p>{tableData[1].no}</p>
+          </div>
+          <div className="bookName">
+            <p>{tableData[1].bookName}</p>
+          </div>
+          <div className="readDate">
+            <p>{tableData[1].readDate}</p>
+          </div>
         </div>
       </div>
     </div>
