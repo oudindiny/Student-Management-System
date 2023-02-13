d7f284cc437424713548aa00b87e30eeacae53ac
yujin <miru93198@gmail.com>
2023-02-13 17:20:33 +0900

    라우터 설정

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import "./App.css";
 import MainPage from "./pages/MainPage";
+import InputPhoneNunber from "./pages/InputPhoneNunber";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <MainPage />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<InputPhoneNunber />} />
+          <Route path="/Main" element={<MainPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/InputPhoneNunber.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const InputPhoneNunber = () => {
+  return;
+};
+export default InputPhoneNunber;
