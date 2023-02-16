e7fa7974aba08271bf9842db887a8e8692e4a322
yujin <miru93198@gmail.com>
2023-02-16 13:19:17 +0900

    inputNumberPage

--- src/pages/InputPhoneNunber.js
+++ src/pages/InputPhoneNunber.js
@@ -1,6 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const InputPhoneNunber = () => {
-  return;
+  const [phoneNumber, setphoneNumber] = useState();
+
+  const handlePhoneNumber = (it) => {
+    return (phoneNumber) => {
+      setphoneNumber([...phoneNumber], `${it}`);
+    };
+  };
+  console.log(phoneNumber);
+
+  return (
+    <div className="InputPhoneNunber">
+      <div className="input">
+        <input></input>
+      </div>
+      {/* <div className="number">
+        <input type="button" id="1" onClick={handlePhoneNumber(this.id)} />
+      </div> */}
+      <button
+        id="1"
+        onClick={() => {
+          handlePhoneNumber("1");
+        }}
+      >
+        1
+      </button>
+    </div>
+  );
 };
 export default InputPhoneNunber;
