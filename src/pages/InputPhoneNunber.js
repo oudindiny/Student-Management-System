d24241bec9f00c85377d6cf7c3ec82c50894868d
yujin <miru93198@gmail.com>
2023-02-17 15:30:29 +0900

    inputPhoneNumber button

--- src/pages/InputPhoneNunber.js
+++ src/pages/InputPhoneNunber.js
@@ -1,15 +1,12 @@
 import React, { useEffect, useState } from "react";
 
 const InputPhoneNunber = () => {
-  const [phoneNumber, setphoneNumber] = useState();
+  const [phoneNumber, setphoneNumber] = useState([]);
 
-  const handlePhoneNumber = (it) => {
-    return (phoneNumber) => {
-      setphoneNumber([...phoneNumber], `${it}`);
-    };
+  const handlePhoneNumber = (e) => {
+    setphoneNumber([...phoneNumber, e.target.innerText]);
   };
   console.log(phoneNumber);
-
   return (
     <div className="InputPhoneNunber">
       <div className="input">
@@ -18,14 +15,17 @@ const InputPhoneNunber = () => {
       {/* <div className="number">
         <input type="button" id="1" onClick={handlePhoneNumber(this.id)} />
       </div> */}
-      <button
-        id="1"
-        onClick={() => {
-          handlePhoneNumber("1");
-        }}
-      >
-        1
-      </button>
+      <span>{phoneNumber}</span>
+      <button onClick={handlePhoneNumber}>1</button>
+      <button onClick={handlePhoneNumber}>2</button>
+      <button onClick={handlePhoneNumber}>3</button>
+      <button onClick={handlePhoneNumber}>4</button>
+      <button onClick={handlePhoneNumber}>5</button>
+      <button onClick={handlePhoneNumber}>6</button>
+      <button onClick={handlePhoneNumber}>7</button>
+      <button onClick={handlePhoneNumber}>8</button>
+      <button onClick={handlePhoneNumber}>9</button>
+      <button onClick={handlePhoneNumber}>0</button>
     </div>
   );
 };
